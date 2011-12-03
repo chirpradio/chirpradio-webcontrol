57440e8bf0ef9a157cd4004d68dac199270e4e67
Adam Cezar Jenkins <emperorcezar@gmail.com>
2011-12-02 22:41:28 -0600

    Basic templates added

--- apps/webcontrol/views.py
+++ apps/webcontrol/views.py
@@ -1,4 +1,4 @@
 from utils.render import render_to_response
 
 def control_panel(request):
-    return render_to_response('base.html', locals())
+    return render_to_response('two-column.html', locals())
