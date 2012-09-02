5a5a19fa9eb565ba6ca93b65f487297eb9d86842
Trevor Borg <trevor.borg@gmail.com>
2012-09-01 19:24:56 -0500

    internal server error handling with traceback report in debug mode / warning in production; debug mode toggle in app.js; moved messages to mainController instead of server

--- server.py
+++ server.py
@@ -8,25 +8,17 @@ import os
 from settings_local import *
 
 PROCESS_ARGUMENTS = {
-    "dump-new-artists-in-dropbox": ['do_dump_new_artists_in_dropbox'],
+    "dump-new-artists-in-dropbox":          ['do_dump_new_artists_in_dropbox'],
     "actually-dump-new-artists-in-dropbox": ['do_dump_new_artists_in_dropbox', '--rewrite'],
-    "do-periodic-import": ['do_periodic_import'],
-    "actually-do-periodic-import": ['do_periodic_import', '--actually-do-import'],
-    "generate-collection-nml": ['do_generate_collection_nml'],
-    "push-artists-to-chirpradio": ['do_push_artists_to_chirpradio'],
-    "push-albums-and-tracks-to-chirpradio": ['do_push_to_chirpradio.py'],
-    "diff-artist-whitelist": ['git', 'diff', 'artist-whitelist'],
-    "commit-artist-whitelist": ['git', 'commit', 'artist-whitelist', 'm', 'Adding new artists'],
-    "push-artist-whitelist": ['git', 'push'],
-    "install-collection-nml": ['install', '-m', '0775', '-g', 'traktor', 'output.nml', TRAKTOR_PATH + '/new-collection.nml']
-}
-
-PROCESS_MESSAGES = {
-    # process-specific commentary you may want to share with the user goes here
-    'dump-new-artists-in-dropbox': 'Carefully proofread the list of new artists. If they are all correct, proceed.',
-    'do-periodic-import': "Do not proceed if there are errors. If you can't resolve them, just move the culprit album aside temporarily.",
-    'actually-do-periodic-import': 'After you have completed this step, you can clean out the dropbox. Refer to the documentation.',
-    'install-collection-nml': 'After this step has been completed, Traktor can be switched over to the new collection: Shut down Traktor, rename new-collection.nml to collection.nml and restart Traktor.'
+    "do-periodic-import":                   ['do_periodic_import'],
+    "actually-do-periodic-import":          ['do_periodic_import', '--actually-do-import'],
+    "generate-collection-nml":              ['do_generate_collection_nml'],
+    "push-artists-to-chirpradio":           ['do_push_artists_to_chirpradio'],
+    "push-albums-and-tracks-to-chirpradio": ['do_push_to_chirpradio'],
+    "diff-artist-whitelist":                ['git', 'diff', 'chirp/library/data/artist-whitelist'],
+    "commit-artist-whitelist":              ['git', 'commit', 'chirp/library/data/artist-whitelist', 'm', 'Adding new artists'],
+    "push-artist-whitelist":                ['git', 'push'],
+    "install-collection-nml":               ['install', '-m', '0775', '-g', 'traktor', 'output.nml', TRAKTOR_PATH + '/new-collection.nml']
 }
 
 app = Flask(__name__)
@@ -43,8 +35,7 @@ def index():
 
 @app.route('/<process_name>')
 def start_process(process_name):
-    args = PROCESS_ARGUMENTS[process_name]
-    message = PROCESS_MESSAGES.get(process_name, False)
+    args = PROCESS_ARGUMENTS.get(process_name, False)
     if args:
         if process_name == 'push-albums-and-tracks-to-chirpradio':
             # this is the only case that requires a run-time variable
@@ -52,17 +43,17 @@ def start_process(process_name):
             args.append('--start-at=' + timestamp)
         proc = desub.join(args)
         if proc.is_running():
-            return jsonify({'started': False, 'error': 'process is already running'})
+            return jsonify({'started': False, 'err': 'process is already running'})
         else:
             proc.start()
-            return jsonify({'started': True, 'message': message})
+            return jsonify({'started': True})
     else:
-        return jsonify({'started': False, 'error': 'unrecognized process name: ' + process_name})
+        return jsonify({'started': False, 'err': 'unrecognized process name: ' + process_name})
 
 
 @app.route('/polling/<process_name>')
 def poll(process_name):
-    args = PROCESS_ARGUMENTS[process_name]
+    args = PROCESS_ARGUMENTS.get(process_name, False)
     if args:
         if process_name == 'push-albums-and-tracks-to-chirpradio':
             # this could potentially backfire if you happen to start the process in
@@ -70,12 +61,13 @@ def poll(process_name):
             timestamp = ''.join(datetime.date.today().isoformat().split('-')) + '-000000'
             args.append('--start-at=' + timestamp)
         proc = desub.join(args)
-        if proc.is_running():
-            return jsonify({'err': proc.stderr.read(), 'out': proc.stdout.read(), 'is_done': False})
-        else:
-            return jsonify({'err': proc.stderr.read(), 'out': proc.stdout.read(), 'is_done': True})
+        return jsonify({
+            'err': proc.stderr.read(),
+            'out': proc.stdout.read(),
+            'is_done': not proc.is_running()
+        })
     else:
-        return jsonify({'started': False, 'error': 'unrecognized process name: ' + process_name})
+        return jsonify({'server_error': 'unrecognized process name: ' + process_name})
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
--- static/js/app.js
+++ static/js/app.js
@@ -1,195 +1,226 @@
 App = Em.Application.create();
 
+App.DEBUG = true;
+
 App.Process = Em.Object.extend({
-        name: null,
-        out: null,
-        err: null,
-        message: null,
-        done: false,
+  name  : null,
+  out   : null,
+  err   : null,
+  done  : false,
 
-        has_output: function() {
-                return (this.get('out') || this.get('err')) ? true : false;
-        }.property('out', 'err'),
+  has_output: function() {
+    return (this.get('out') || this.get('err')) ? true : false;
+  }.property('out', 'err'),
 
-        space_separated: function() {
-                return this.get('name').replace(/-/g, ' ');
-        }.property('name'),
+  space_separated: function() {
+    return this.get('name').replace(/-/g, ' ');
+  }.property('name'),
 
-        path: function() {
-                return '/' + this.get('name');
-        }.property('name'),
+  path: function() {
+    return '/' + this.get('name');
+  }.property('name'),
 
-        polling_path: function() {
-                return '/polling' + this.get('path');
-        }.property('path'),
+  polling_path: function() {
+    return '/polling' + this.get('path');
+  }.property('path'),
 
-        parse_response: function(response) {
-                this.set('out', response.out);
-                this.set('err', response.err);
-        }
+  parse_response: function(response) {
+    this.set('out', response.out);
+    this.set('err', response.err);
+  }
 });
 
 App.ApplicationController = Em.ObjectController.extend({
-        live_run_available: false
+  // Change this setting to enable the live run option.
+  live_run_available: !App.DEBUG
 });
 
 App.ApplicationView = Em.View.extend({
-        classNames: ['wrapper'],
-        templateName: 'app'
+  classNames  : ['wrapper'],
+  templateName: 'app'
 });
 
 App.SplashView = Em.View.extend({
-        templateName: 'splash'
+  templateName: 'splash'
 });
 
 App.MainController = Em.ObjectController.extend({
-        // set by router
-        content: null,
-        processes: null,
+  // set by router
+  content   : null,
+  processes : null,
 
-        // computed properties
-        current_step_index: function() {
-                return this.get('processes').indexOf(this.get('content.name'));
-        }.property('content', 'processes'),
+  messages: {
+    'dump-new-artists-in-dropbox' :  'Carefully proofread the list of new artists. If they are all correct, proceed.',
+    'do-periodic-import'          :  "Do not proceed if there are errors. If you can't resolve them, just move the culprit album aside temporarily.",
+    'actually-do-periodic-import' :  'After you have completed this step, you can clean out the dropbox. Refer to the documentation.',
+    'install-collection-nml'      :  'After this step has been completed, Traktor can be switched over to the new collection: Shut down Traktor, rename new-collection.nml to collection.nml and restart Traktor.'
+  },
 
-        next_step_name: function() {
-                return this.get('processes')[this.get('current_step_index') + 1];
-        }.property('processes', 'current_step_index'),
+  // computed properties
+  current_step_index: function() {
+    return this.get('processes').indexOf(this.get('content.name'));
+  }.property('content', 'processes'),
 
-        available_steps: function() {
-                var procs = this.get('processes').copy().splice(0, this.get('current_step_index'));
-                return procs.map(function(name) { return App.Process.create({name: name}); });
-        }.property('processes', 'current_step_index'),
+  next_step_name: function() {
+    return this.get('processes')[this.get('current_step_index') + 1];
+  }.property('processes', 'current_step_index'),
 
-        is_not_last: function() {
-                return (this.get('current_step_index') !== this.get('processes').length - 1);
-        }.property('current_step_index', 'processes'),
+  available_steps: function() {
+    var procs = this.get('processes').copy().splice(0, this.get('current_step_index'));
+    return procs.map(function(name) { return App.Process.create({name: name}); });
+  }.property('processes', 'current_step_index'),
 
-        // methods
-        step_is_available: function(step) {
-                return this.get('processes').indexOf(step) < this.get('current_step_index');
-        }
+  is_not_last: function() {
+    return (this.get('current_step_index') !== this.get('processes').length - 1);
+  }.property('current_step_index', 'processes'),
+
+  message: function() {
+    return this.get('messages')[this.get('content.name')] || false;
+  }.property('content.name'),
+
+  // methods
+  step_is_available: function(step) {
+    return this.get('processes').indexOf(step) < this.get('current_step_index');
+  }
 });
 
 App.MainView = Em.View.extend({
-        templateName: 'main'
+  templateName: 'main'
 });
 
 var router = Em.Router.create({
-        enableLogging: true,
+  enableLogging: App.DEBUG,
 
-        root: Em.Route.extend({
+  root: Em.Route.extend({
 
-                splash: Em.Route.extend({
-                        route: '/',
+    splash: Em.Route.extend({
+            route: '/',
 
-                        do_live: function(router) {
-                                var context = App.Process.create({name: 'dump-new-artists-in-dropbox'});
-                                router.get('mainController').set('processes', [
-                                        "dump-new-artists-in-dropbox",
-                                        "actually-dump-new-artists-in-dropbox",
-                                        "diff-artist-whitelist",
-                                        "commit-artist-whitelist",
-                                        "push-artist-whitelist",
-                                        "do-periodic-import",
-                                        "actually-do-periodic-import",
-                                        "generate-collection-nml",
-                                        "install-collection-nml",
-                                        "push-artists-to-chirpradio",
-                                        "push-albums-and-tracks-to-chirpradio"
-                                ]);
-                                router.transitionTo('main', context);
-                        },
+            do_live: function(router) {
+                    var context = App.Process.create({name: 'dump-new-artists-in-dropbox'});
+                    router.get('mainController').set('processes', [
+                            "dump-new-artists-in-dropbox",
+                            "actually-dump-new-artists-in-dropbox",
+                            "diff-artist-whitelist",
+                            "commit-artist-whitelist",
+                            "push-artist-whitelist",
+                            "do-periodic-import",
+                            "actually-do-periodic-import",
+                            "generate-collection-nml",
+                            "install-collection-nml",
+                            "push-artists-to-chirpradio",
+                            "push-albums-and-tracks-to-chirpradio"
+                    ]);
+                    router.transitionTo('main', context);
+            },
 
-                        serialize: function() {
-                                return '';
-                        },
+            serialize: function() {
+                    return '';
+            },
 
-                        do_read_only: function(router) {
-                                var context = App.Process.create({name: 'dump-new-artists-in-dropbox'});
-                                router.get('mainController').set('processes', [
-                                        "dump-new-artists-in-dropbox",
-                                        "do-periodic-import"
-                                ]);
-                                router.transitionTo('main', context);
-                        },
+            do_read_only: function(router) {
+                    var context = App.Process.create({name: 'dump-new-artists-in-dropbox'});
+                    router.get('mainController').set('processes', [
+                            "dump-new-artists-in-dropbox",
+                            "do-periodic-import"
+                    ]);
+                    router.transitionTo('main', context);
+            },
 
-                        connectOutlets: function(router) {
-                                router.get('applicationController').connectOutlet('splash');
-                        }
-                }),
+            connectOutlets: function(router) {
+                    router.get('applicationController').connectOutlet('splash');
+            }
+    }),
 
-                main: Em.Route.extend({
-                        route: '/:step',
+    main: Em.Route.extend({
+      route: '/:step',
 
-                        go_to_step: function(router, context) {
-                                router.transitionTo('main', App.Process.create({name: context.context}));
-                        },
+      go_to_step: function(router, context) {
+        router.transitionTo('main', App.Process.create({name: context.context}));
+      },
 
-                        connectOutlets: function(router, context) {
-                                if (!router.get('mainController.processes')) {
-                                        return router.transitionTo('splash');
-                                }
-                                router.get('applicationController').connectOutlet('main', context);
-                        },
+      connectOutlets: function(router, context) {
+        if (!router.get('mainController.processes')) {
+                return router.transitionTo('splash');
+        }
+        router.get('applicationController').connectOutlet('main', context);
+      },
 
-                        serialize: function(router, context) {
-                                return { step: context.get('name') };
-                        },
+      serialize: function(router, context) {
+        if (context) {
+          return { step: context.get('name') };
+        }
+      },
 
-                        deserialize: function(router, params) {
-                                return App.Process.create({ name: params.step });
-                        },
+      deserialize: function(router, params) {
+        return App.Process.create({ name: params.step });
+      },
 
-                        // child states
-                        initialState: 'ready',
+      // child states
+      initialState: 'ready',
 
-                        ready: Em.Route.extend({
-                                route: '/',
+      ready: Em.Route.extend({
+        route: '/',
 
-                                start_process: function(router, context) {
-                                        router.transitionTo('polling');
-                                        $.get(context.context.get('path'), function(response) {
-                                                        context.context.set('message', response.message);
-                                                        router.send('poll', context.context);
-                                        });
+        start_process: function(router, context) {
+          router.transitionTo('polling');
+          $.get(context.context.get('path'), function(response) {
+            if (!response.started) context.context.set('err', response.error);
+            router.send('poll', context.context);
+          })
+          .error(function(err) {
+            if (App.DEBUG) {
+              return $('html').html(err.responseText);
+            }
+            context.context.set('err', $(err.responseText).text());
+          });
+        }
+      }),
 
-                                }
-                        }),
+      polling: Em.Route.extend({
+        route: '/polling',
 
-                        polling: Em.Route.extend({
-                                route: '/polling',
+        poll: function(router, context) {
+          $.get(context.get('polling_path'), function(response) {
+            if (response.hasOwnProperty('server_error')) {
+              context.set('err', response.server_error);
+              router.transitionTo('completed');
+              return;
+            }
+            context.parse_response(response);
+            if (response.is_done) {
+              context.set('done', true);
+              router.transitionTo('completed');
+            } else {
+              router.send('poll', context);
+            }
+          })
+          .error(function(err) {
+            if (App.DEBUG) {
+              return $('html').html(err.responseText);
+            }
+            context.set('err', $(err.responseText).text());
+          });
+        },
 
-                                poll: function(router, context) {
-                                        $.get(context.get('polling_path'), function(response) {
-                                                context.parse_response(response);
-                                                if (response.is_done) {
-                                                        context.set('done', true);
-                                                        router.transitionTo('completed');
-                                                } else {
-                                                        router.send('poll', context);
-                                                }
-                                        });
-                                },
+        complete_process: Em.Route.transitionTo('completed')
+      }),
 
-                                complete_process: Em.Route.transitionTo('completed')
-                        }),
+      completed: Em.Route.extend({
+        route: '/done',
 
-                        completed: Em.Route.extend({
-                                route: '/done',
+        start_process: function(router, context) {
+                router.transitionTo('ready');
+                router.send('start_process', context);
+        },
 
-                                start_process: function(router, context) {
-                                        router.transitionTo('ready');
-                                        router.send('start_process', context);
-                                },
-
-                                go_to_next_step: function(router) {
-                                        var context = App.Process.create({name: router.get('mainController.next_step_name')});
-                                        router.transitionTo('main', context);
-                                }
-                        })
-                })
-        })
+        go_to_next_step: function(router) {
+                var context = App.Process.create({name: router.get('mainController.next_step_name')});
+                router.transitionTo('main', context);
+        }
+      })
+    })
+  })
 });
 
 App.initialize(router);
